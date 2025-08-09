04751fe6f2c7bcf6021b22b6fee76dab5ea56b00
Michael Juliano <mdjules@gmail.com>
2025-08-09 11:39:15 -0400

    Create list of menu items with images and names
    Using a for each to loop through an array of menu items
    which are objects that have a name and a src image.
    Create a container for each. This is all included in the
    original loadMenu function

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,8 @@
+import image1 from "./images/spaghetti.jpg"
+import image2 from "./images/chicken-parm.jpg"
+import image3 from "./images/caesar.jpg"
+
+
 export function loadMenu() {
     const content = document.getElementById("content");
     const button = document.querySelector(".menuButton");
@@ -5,14 +10,32 @@ export function loadMenu() {
 
         content.innerHTML = ""
         const menuContainer = document.createElement('div');
-        menuContainer.textContent = "test"
+        menuContainer.classList.add("menuContainer")
         content.appendChild(menuContainer)
 
-        for (let i = 0; i < 3; i++) {
-            const items = document.createElement('div');
-            items.textContent = `Item ${i + 1}`
-            items.classList.add(`Item-${i + 1}`, "menu-item")
-            menuContainer.appendChild(items)
-        }
+        const menuItems = [
+            {name: "Spaghetti and Meatballs", image: image1},
+            {name: "Chicken Parm", image: image2},
+            {name: "Caesar Salad", image: image3}   
+
+        ];
+
+        menuItems.forEach(item  => {
+            const itemContainer = document.createElement('div')
+            itemContainer.classList.add('itemContainer')
+
+            const img = document.createElement('img');
+            img.src = item.image;
+            img.classList.add('menu-image');
+
+            const name = document.createElement('h3');
+            name.textContent = item.name;
+
+            itemContainer.appendChild(name);
+            itemContainer.appendChild(img);
+            menuContainer.appendChild(itemContainer);
+
+            
+        })  
     })
 }
