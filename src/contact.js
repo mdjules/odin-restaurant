42c15c2d54e1364c8218f85af8c97ee1a1612e3d
Michael Juliano <mdjules@gmail.com>
2025-08-17 16:02:58 -0400

    Add contact info and style the contact info page

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,4 @@
+
 export function loadContact() {
     const content = document.getElementById('content')
     const button = document.querySelector(".contactButton")
@@ -5,8 +6,29 @@ export function loadContact() {
     button.addEventListener('click', function() {
         content.innerHTML = ""
 
-        const container = document.createElement('div')
-        container.textContent = "All about my cool restaurant"
-        content.appendChild(container);
+        const contactContainer = document.createElement('div')
+        contactContainer.classList.add('contactContainer')
+        content.appendChild(contactContainer);
+
+        const info = 
+            {
+                address: "123 That Guy Lane",
+                state: "TG",
+                email: "thatguy@italianrestaurant.com",
+                phone: '123-456-7890'
+            }
+    
+
+        const contactInfo = document.createElement('div')
+        contactInfo.classList.add('contactInfo')
+        
+        Object.keys(info).forEach(key => {
+            const value = info[key];
+            const p = document.createElement('p')
+            p.textContent = `${key.toUpperCase()}: ${value}`;
+            contactInfo.appendChild(p)
+        })
+
+        contactContainer.appendChild(contactInfo)
     })
 }
